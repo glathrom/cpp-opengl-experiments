585ba43b0bab711ba3b03c5cf0c4b282f8677f0f
glathrom <grant.lathrom@gmail.com>
2021-08-12 22:36:05 -0500

    added opengl attribute description

--- src/application.cpp
+++ src/application.cpp
@@ -7,6 +7,10 @@ int main(void){
 
     GLFWwindow* window;
 
+    /* ******************************************************** */
+    // creates an opengl window so that glew has something
+    // at which to point 
+    
     if(!glfwInit())
         return -1;
 
@@ -18,19 +22,60 @@ int main(void){
 
     glfwMakeContextCurrent(window);
     
+    /* ******************************************************** */
+    
     if(glewInit() != GLEW_OK)
         std::cout << "Error with glewInit" << std::endl;
+
+    // the positions for the triangle
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.5f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    /////////////////////////////////////////////////////////////
+    // creates a buffer of data on which opengl can operate
+    /////////////////////////////////////////////////////////////
+    
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+
+
+    /////////////////////////////////////////////////////////////
+    // creates an attribute array which tells opengl
+    // how the buffer created above is supposed to be parsed
+    // essentially this is telling opengl how the data is going
+    // to be structured - defining a STRUCT in C for example
+    /////////////////////////////////////////////////////////////
+
+    // the index is actually the index of the pointer 
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
+
+
+    /////////////////////////////////////////////////////////////
+    // creates a shader which is basically a source program
+    // which must then be compiled and set to the GPU
+    // using the struct and the data which are defined above
+    /////////////////////////////////////////////////////////////
+
 
     while(!glfwWindowShouldClose(window)){
 
         glClear(GL_COLOR_BUFFER_BIT);
 
+        /*
         glBegin(GL_TRIANGLES);
-        
         glVertex2f(-0.5f, -0.5f);
         glVertex2f( 0.5f,  0.5f);
         glVertex2f( 0.5f, -0.5f);
         glEnd();
+        */
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents(); 
