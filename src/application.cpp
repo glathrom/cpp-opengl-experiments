5e9160797a8c9ea079843fad3a34a9f0eda964b1
grant <grant.lathrom@gmail.com>
2021-08-29 21:06:39 -0500

    ready to add uniforms

--- src/application.cpp
+++ src/application.cpp
@@ -1,9 +1,22 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <c++/9/iostream>
-#include <c++/9/fstream>
-#include <c++/9/string>
-#include <c++/9/sstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
+static void GLClearError(){
+    while(glGetError() != GL_NO_ERROR);
+}
+
+static void GLCheckError(){
+    while( GLenum error = glGetError() ){
+        std::cout << "[OpenGL Error] (" << error << ")" << std::endl;
+    }
+}
+
+
 
 const std::string  ReadShader(const std::string& filepath){
 
@@ -79,16 +92,24 @@ int main(void){
 
     glfwMakeContextCurrent(window);
     
+    glfwSwapInterval(1);
+    
     /* ******************************************************** */
     
     if(glewInit() != GLEW_OK)
         std::cout << "Error with glewInit" << std::endl;
 
     // the positions for the triangle
-    float positions[6] = {
+    float positions[8] = {
         -0.5f, -0.5f,
          0.5f,  0.5f,
-         0.5f, -0.5f
+         0.5f, -0.5f,
+        -0.5f,  0.5f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2, 
+        0, 3, 1
     };
 
     /////////////////////////////////////////////////////////////
@@ -98,7 +119,7 @@ int main(void){
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 4*2*sizeof(float), positions, GL_STATIC_DRAW);
 
 
     /////////////////////////////////////////////////////////////
@@ -111,6 +132,14 @@ int main(void){
     // the index is actually the index of the pointer 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
+    
+    
+    // using an index buffer to render the object 
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6*sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 
     /////////////////////////////////////////////////////////////
@@ -125,13 +154,31 @@ int main(void){
     unsigned int shader = CreateShader(vertexShader, fragmentShader);
     glUseProgram(shader);
 
+    float r = 0.0f;
+    float increment = 0.05f;
+    int location = glGetUniformLocation(shader, "u_Color");
+    glUniform4f(location, r, 0.2f, 0.9f, 1.0f);
+    GLCheckError();
 
+    
 
     while(!glfwWindowShouldClose(window)){
 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GLCheckError();
+
+        if( r > 1.0f ){
+            increment = -0.05f;
+        } else if (r < 0.0f){
+            increment = 0.05f;        
+        }
+
+        r += increment;
+    
+        glUniform4f(location, r, 0.2f, 0.9f, 1.0f);
 
         glfwSwapBuffers(window);
         
